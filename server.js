fafc6739680fdc0c0bfbb8e5ffe7e76b5222561f
zoritto <zoritto@zoritto.zoritto>
2024-10-17 15:14:15 -0300

    Editando o usuario no banco de dados

--- server.js
+++ server.js
@@ -3,20 +3,33 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
-const users = [];
 app.post('/usuarios', async (req, res) => {
     await prisma.user.create({
-         data:{
-            email:req.body.email,
-            name:req.body.name,
-            age:req.body.age,
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
         }
     })
     users.push(req.body);
     res.status(201).json(req.body);
 });
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+    const users = await prisma.user.findMany()
     res.status(200).json(users);
+});
+app.put('/usuarios/:id', async (req, res) => {
+    await prisma.user.update({
+        where:{
+            id:req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
+    res.status(201).json(req.body);
 });
 
 app.listen(3000)
