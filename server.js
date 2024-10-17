9fe01301d22236ddcb9f28845c75445f97b875b9
zoritto <zoritto@zoritto.zoritto>
2024-10-17 14:01:16 -0300

    adicionando informações do usuario

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 const users = [];
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
+    await prisma.user.create({
+         data:{
+            email:req.body.email,
+            name:req.body.name,
+            age:req.body.age,
+        }
+    })
     users.push(req.body);
     res.status(201).json(req.body);
 });
